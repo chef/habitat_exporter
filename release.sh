31d25887f0cad6a156d87782dbd0c7c109cf284c
Mark Harrison <mark@mivok.net>
2018-06-11 16:42:01 -0400

    Release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERSION=$(grep Version version.go | awk -F'"' '{print $2}')
+echo "=> Version: $VERSION"
+
+PROGNAME="${PWD##*/}"
+
+echo "=> Tagging current release"
+git tag "$VERSION"
+echo "=> Pushing tag"
+git push origin "$VERSION"
+
+goreleaser
